c916a820e5d9d0d39a2389fa385a9dce0b020a28
VitaliyDronenko <buy-matras@yandex.ru>
2023-09-04 22:12:43 +0300

    77777

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        int i = 1;
-        System.out.println(8);
-        System.out.println(55);
-        String str = new String("12345!");
-        System.out.println(str);
-        System.out.println(2);
-        System.out.println(55);
-        String str = new String("12345!");
-        System.out.println(str);
+
     }
 }
