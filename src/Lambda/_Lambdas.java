f06c684078b41263632d9734810409cdb16a2664
VitaliyDronenko <buy-matras@yandex.ru>
2023-09-05 21:46:28 +0300

    toUpper

--- src/Lambda/_Lambdas.java
+++ src/Lambda/_Lambdas.java
@@ -1,15 +1,35 @@
 package Lambda;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Function;
 
 public class _Lambdas {
     public static void main(String[] args) {
-        Function<String, String> upperCase = (name) -> {
-            if (name.isBlank()) throw  new IllegalArgumentException();
-            return name.toUpperCase();
-        };
-        String str = upperCase.apply("vitos");
-        System.out.println(str);
+        System.out.println("Hello");
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("vika"));
+        list.add(new Person("vitos"));
+        list.stream()
+                .map(e->e.getName().toUpperCase()).forEach(System.out::println);
+    }
+
+    static class Person {
+        private final String name;
+
+        public Person(String name) {
+            this.name = name;
         }
 
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    '}';
+        }
     }
+}
