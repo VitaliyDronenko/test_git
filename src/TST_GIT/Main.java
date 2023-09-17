209be45781ea812c4f91ddcbc94bbc4ed25b3276
VitaliyDronenko <100946640+VitaliyDronenko@users.noreply.github.com>
2023-09-17 15:18:01 +0300

    Update Main.java
    Add message Conflict

--- src/TST_GIT/Main.java
+++ src/TST_GIT/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HI Corp 2+2");
         System.out.println("Conflict!");
+        System.out.println("Conflict!");
     }
 }
